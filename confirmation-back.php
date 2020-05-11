cb217f5e49aa8b4eb8929fa4202b51ed80ad4c14
belcaid abderrahim <belcaidna@gmail.com>
2020-05-11 14:52:56 +0000

    version 1.3

--- confirmation-back.php
+++ confirmation-back.php
@@ -2,8 +2,8 @@
 include('dbconnection.php');
 
 
-// if(isset($_GET['pid'])){
-//     $id= $_GET['pid'];
+if(isset($_GET['pid'])){
+    $id= $_GET['pid'];
 
     
 $query= "SELECT *  FROM passager ORDER BY id DESC LIMIT 1";
@@ -11,19 +11,12 @@ $stmt =$conn->prepare($query);
 $stmt->execute();
 $result= $stmt->get_result();
 $roow = $result->fetch_assoc();
-$stmt->close();
-
-$query= "SELECT *  FROM tmp ORDER BY id DESC LIMIT 1";
-$stmt =$conn->prepare($query);
-$stmt->execute();
-$result1= $stmt->get_result();
-$roowtmp = $result1->fetch_assoc();
 
 
 // echo $roow['id'];
  $date = date('Y-m-d H:i:s');
 $stmt = $conn->prepare("INSERT Into reservation (vol_id,passager_id,date_reservation) values(?,?,?)");
-$stmt->bind_param("iis",$roowtmp['id'],$roow['id'],$date);
+$stmt->bind_param("iis",$id,$roow['id'],$date);
 $stmt->execute();
 $stmt->close();
 
@@ -31,7 +24,7 @@ $stmt->close();
 
 $query= "SELECT * FROM vols WHERE id=?";
 $stmt = $conn->prepare($query);
-$stmt->bind_param("i",$roowtmp['id']);
+$stmt->bind_param("i",$id);
 $stmt->execute();
 $result= $stmt->get_result();
 $row1 = $result->fetch_assoc();
@@ -50,7 +43,8 @@ $row2 = $result->fetch_assoc();
 // echo $row2['nom'];
 
     
-// }
+}
+
 
 
 
